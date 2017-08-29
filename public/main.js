e068db3de668ae8f5f96e2c3a43f327353637cdd
Nina Bulavina <rat55@ngs.ru>
2017-08-29 20:21:03 +0300

    fix style for mobile

--- public/main.js
+++ public/main.js
@@ -1,15 +1,21 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+	console.log(document.getElementById("phone"));
+	var cleave = new Cleave(document.getElementById("phone"), {
+	    phone: true,
+	    phoneRegionCode: 'RU'
+	});
 
-
-function modalFunction(){
-	var element = document.getElementById("modalf");
-	var shadeElem = document.getElementById("shade");
-  	element.classList.add("visible");
-  	shadeElem.classList.add("visible");
-}
-  
-function disableModal(){
-	var element = document.getElementById("modalf");
-	var shadeElem = document.getElementById("shade");
-  	element.classList.remove("visible");
-  	shadeElem.classList.remove("visible");
-}
+	function modalFunction(){
+		var element = document.getElementById("modalf");
+		var shadeElem = document.getElementById("shade");
+	  	element.classList.add("visible");
+	  	shadeElem.classList.add("visible");
+	}
+	  
+	function disableModal(){
+		var element = document.getElementById("modalf");
+		var shadeElem = document.getElementById("shade");
+	  	element.classList.remove("visible");
+	  	shadeElem.classList.remove("visible");
+	}
+});
