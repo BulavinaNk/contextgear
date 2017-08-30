fc91f34561fcf155b6145fefdabc0c5ec62d0b87
Nina Bulavina <rat55@ngs.ru>
2017-08-30 15:24:59 +0300

    fix bag

--- public/main.js
+++ public/main.js
@@ -1,3 +1,17 @@
+function modalFunction(){
+	var element = document.getElementById("modalf");
+	var shadeElem = document.getElementById("shade");
+  	element.classList.add("visible");
+  	shadeElem.classList.add("visible");
+}
+  
+function disableModal(){
+	var element = document.getElementById("modalf");
+	var shadeElem = document.getElementById("shade");
+  	element.classList.remove("visible");
+  	shadeElem.classList.remove("visible");
+}
+
 document.addEventListener("DOMContentLoaded", function(event) { 
 	console.log(document.getElementById("phone"));
 	var cleave = new Cleave(document.getElementById("phone"), {
@@ -5,17 +19,5 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	    phoneRegionCode: 'RU'
 	});
 
-	function modalFunction(){
-		var element = document.getElementById("modalf");
-		var shadeElem = document.getElementById("shade");
-	  	element.classList.add("visible");
-	  	shadeElem.classList.add("visible");
-	}
-	  
-	function disableModal(){
-		var element = document.getElementById("modalf");
-		var shadeElem = document.getElementById("shade");
-	  	element.classList.remove("visible");
-	  	shadeElem.classList.remove("visible");
-	}
+	
 });
